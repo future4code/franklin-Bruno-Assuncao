b32b8150a2f8a5ee96d5daa2d0407833102b7183
Bruno Pereira Assuncao <bruno.assuncao@grupoboticario.com.br>
2022-08-28 21:05:07 -0300

    Atualizando endpoints

--- modulo3/To-Do-List/src/app.ts
+++ modulo3/To-Do-List/src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
@@ -16,4 +17,4 @@ const server = app.listen(process.env.PORT || 3003, () => {
   }
 });
 
-export default app
+export default app;
